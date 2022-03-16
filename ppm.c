03a65d909223b6a82db584579350ac5272f4f4ea
pomjak <pomjak71@gmail.com>
2022-03-17 00:41:26 +0100

    steg progress++

--- ppm.c
+++ ppm.c
@@ -1,9 +1,18 @@
 #include "ppm.h"
 
+struct ppm 
+{
+    unsigned xsize;
+    unsigned ysize;
+    char data[];    // RGB bajty, celkem 3*xsize*ysize
+};
+
+
 struct ppm * ppm_read(const char * filename)
 {
-    FILE *ppm_file = fopen(filename, "rb");
-    if (!ppm_file){error_exit("");}
+    char buffer[99];
+    //fscanf(filename,buffer)
+    return 0;   
 }
 
 
--- steg-decode.c
+++ steg-decode.c
@@ -6,9 +6,11 @@
 
  int main(int argc, char *argv[])
 {
-    if(argc > 1)
+    if(argc != 2)
     {
-
+        FILE *ppm_file = fopen(argv[1], "rb");
+        if (!ppm_file){error_exit("steg-decode:Soubor nebyl nalezen\n");}
+        struct ppm *image = ppm_read(ppm_file); 
     }
     else error_exit("steg-decode:Malo zadanych argumentu!\n");
     return 0;
